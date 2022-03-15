10e7200d95cbb144007852989027f7fefc931673
redbuilderOne <forsaken2492@yandex.ru>
2022-03-15 17:06:20 +0300

    re

--- Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -1,13 +1,26 @@
-//
-//  ViewController.swift
-//  Netology_IB_Instruments
-//
-//  Created by Дмитрий Скворцов on 15.02.2022.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-    
 
+    @IBOutlet weak var viewForXibFile: ProfileView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        view.addSubview(ProfileView())
+    }
+
+    private func loadViewFromXib() -> UIView {
+        guard let xibView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else { return UIView() }
+
+        return xibView
+    }
+
+    private func setupView() {
+        let xibView = self.loadViewFromXib()
+//        xibView.frame = view.bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(xibView)
+    }
 }
