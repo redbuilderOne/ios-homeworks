4629921b7dbdf8b8832a1adae8a5483b92b6e1b7
redbuilderOne <forsaken2492@yandex.ru>
2022-03-31 11:23:53 +0300

    reCollectionView_v5

---
+++ CollectionView_v5/Navigation/PostTableViewCell.swift
@@ -0,0 +1,140 @@
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    var tableImageView = UIImageView()
+    var tableViewTitleLabel = UILabel()
+    var tableViewDescriptionLabel = UILabel()
+    var tableViewLikesLabel = UILabel()
+    var tableViewViewsLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        addSubview(tableImageView)
+        addSubview(tableViewTitleLabel)
+        addSubview(tableViewDescriptionLabel)
+        addSubview(tableViewLikesLabel)
+        addSubview(tableViewViewsLabel)
+
+        configureTableImageView()
+        configureTableViewTitleLabel()
+        configureTableViewDescriptionLabel()
+        configureTableViewLikesLabel()
+        configureTableViewViewsLabel()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        tableViewTitleLabel.frame = CGRect(x: 16,
+                                           y: 16,
+                                           width: self.frame.width-16,
+                                           height: 40)
+
+
+        tableImageView.frame = CGRect(x: 0,
+                                      y: 64,
+                                      width: tableImageView.frame.size.width,
+                                      height: 300)
+
+
+        tableViewDescriptionLabel.frame = CGRect(x: 16,
+                                                 y: 46+tableImageView.frame.size.height,
+                                                 width: self.frame.width-16,
+                                                 height: 100)
+
+
+        tableViewLikesLabel.frame = CGRect(x: 16,
+                                           y: 316+tableViewDescriptionLabel.frame.size.height,
+                                           width: self.frame.width,
+                                           height: 40)
+
+
+        tableViewViewsLabel.frame = CGRect(x: 320,
+                                           y: 316+tableViewDescriptionLabel.frame.size.height,
+                                           width: self.frame.width,
+                                           height: 40)
+
+
+        NSLayoutConstraint.activate([
+            tableViewTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            tableViewTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            tableViewTitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            tableViewTitleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+
+            tableImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            tableImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            tableImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            tableImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+
+            tableViewDescriptionLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            tableViewDescriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: -16),
+            tableViewDescriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            tableViewDescriptionLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+
+            tableViewLikesLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            tableViewLikesLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            tableViewLikesLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            tableViewLikesLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+
+            tableViewViewsLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            tableViewViewsLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            tableViewViewsLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            tableViewViewsLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0)
+        ])
+    }
+
+    // присваиваем значения из Post к созданным элементам
+    func set(post: Post) {
+        tableImageView.image = post.image
+        tableViewTitleLabel.text = post.title
+        tableViewDescriptionLabel.text = post.description
+        tableViewLikesLabel.text = "Likes: " + String(post.likes)
+        tableViewViewsLabel.text = "Views: " + String(post.views)
+    }
+
+    private func configureTableImageView() {
+        tableImageView.contentMode = .scaleAspectFit
+        tableImageView.backgroundColor = .black
+        tableImageView.translatesAutoresizingMaskIntoConstraints = false
+        tableImageView.adjustsImageSizeForAccessibilityContentSizeCategory = true
+        tableImageView.clipsToBounds = true
+    }
+
+    private func configureTableViewTitleLabel() {
+        tableViewTitleLabel.font = .boldSystemFont(ofSize: 20)
+        tableViewTitleLabel.textColor = .black
+        tableViewTitleLabel.numberOfLines = 2
+        tableViewTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        tableViewTitleLabel.adjustsFontSizeToFitWidth = true
+        tableViewTitleLabel.setContentCompressionResistancePriority(UILayoutPriority(1000), for: .vertical)
+        tableViewTitleLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+    }
+
+    private func configureTableViewDescriptionLabel() {
+        tableViewDescriptionLabel.font = .systemFont(ofSize: 14)
+        tableViewDescriptionLabel.textColor = .systemGray
+        tableViewDescriptionLabel.numberOfLines = 0
+        tableViewDescriptionLabel.adjustsFontSizeToFitWidth = true
+        tableViewDescriptionLabel.setContentCompressionResistancePriority(UILayoutPriority(1000), for: .vertical)
+        tableViewDescriptionLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+    }
+
+    private func configureTableViewLikesLabel() {
+        tableViewLikesLabel.font = .systemFont(ofSize: 16)
+        tableViewLikesLabel.textColor = .black
+        tableViewLikesLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    private func configureTableViewViewsLabel() {
+        tableViewViewsLabel.font = .systemFont(ofSize: 16)
+        tableViewViewsLabel.textColor = .black
+        tableViewViewsLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ CollectionView_v5/Navigation/ProfileVC.swift
@@ -0,0 +1,219 @@
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    lazy var profileHeaderView = ProfileHeaderView()
+    lazy var headerView = UIView()
+
+    // MARK: - tableView1
+    private lazy var tableView1: UITableView = {
+        let tableView1 = UITableView()
+        tableView1.translatesAutoresizingMaskIntoConstraints = false
+        tableView1.delegate = self
+        tableView1.dataSource = self
+        tableView1.rowHeight = UITableView.automaticDimension
+        tableView1.estimatedRowHeight = 44
+        tableView1.register(PhotoCollectionTableViewCell.self, forCellReuseIdentifier: Cells.phCollectionID)
+        tableView1.register(PostTableViewCell.self, forCellReuseIdentifier: Cells.postCellID)
+        return tableView1
+    }()
+
+    private lazy var layout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        return layout
+    }()
+
+    var postContent: [Post] = []
+    var photoContent: [UIImage] = []
+
+    public struct Cells {
+        static let postCellID = "PostTableViewCell"
+        static let phCollectionID = "PhotoCollectionTableViewCell"
+    }
+
+    // MARK: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        postContent = fetchPostData()
+        photoContent = fetchPhotoData()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        view.backgroundColor = .systemBackground
+        view.addSubview(headerView)
+        configureViewForProfileHeader()
+        headerView.addSubview(profileHeaderView)
+        view.addSubview(tableView1)
+        confTableView()
+        configureProfileHeaderViewUI()
+    }
+
+    // MARK: - Configurations
+
+    private func configureViewForProfileHeader() {
+        headerView.backgroundColor = .systemGray6
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            headerView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            headerView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            headerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            headerView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    private func configureProfileHeaderViewUI() {
+        profileHeaderView.anchor(top: headerView.safeAreaLayoutGuide.topAnchor, height: 220)
+
+        NSLayoutConstraint.activate([
+            profileHeaderView.widthAnchor.constraint(equalTo: headerView.widthAnchor),
+            profileHeaderView.heightAnchor.constraint(equalTo: headerView.heightAnchor),
+            profileHeaderView.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            profileHeaderView.centerYAnchor.constraint(equalTo: headerView.centerYAnchor)
+        ])
+    }
+
+    private func confTableView() {
+        // вкл/выкл тап по таблице
+        tableView1.isUserInteractionEnabled = true
+        tableView1.anchor(top: profileHeaderView.showStatusButton.safeAreaLayoutGuide.topAnchor,
+                         left: headerView.safeAreaLayoutGuide.leftAnchor,
+                         bottom: headerView.safeAreaLayoutGuide.bottomAnchor,
+                         right: headerView.safeAreaLayoutGuide.rightAnchor,
+                         paddingTop: 64)
+
+        NSLayoutConstraint.activate([
+            tableView1.leadingAnchor.constraint(equalTo: headerView.leadingAnchor),
+            tableView1.trailingAnchor.constraint(equalTo: headerView.trailingAnchor),
+            tableView1.bottomAnchor.constraint(equalTo: headerView.bottomAnchor)
+        ])
+    }
+
+    // MARK: - Buttons & Actions
+    func leftArrowButtonPressConfig() {
+        PhotoCollectionTableViewCell.leftArrowButton.addTarget(self, action: #selector(pushPhotoGallery), for: .touchUpInside)
+        print("leftArrowButton is pressed")
+    }
+
+    @objc func pushPhotoGallery() {
+        let photoRootVC = SecondPhotoViewController()
+        lazy var photoNavVC = UINavigationController(rootViewController: photoRootVC)
+        self.navigationController?.pushViewController(photoRootVC, animated: true)
+    }
+
+    @objc func fingerTap() {
+        print("fingerTap was called")
+        lazy var photoRootVC = SecondPhotoViewController()
+        lazy var photoNavVC = UINavigationController(rootViewController: photoRootVC)
+        self.navigationController?.pushViewController(photoRootVC, animated: true)
+    }
+}
+
+// MARK: - fetchData extension
+extension ProfileVC {
+
+    func fetchPostData() -> [Post] {
+        return [firstPost, secondPost, thirdPost, fourthPost]
+    }
+
+    func fetchPhotoData() -> [UIImage] {
+        return previewArrayOfCollectionViewImg
+    }
+}
+
+// MARK: - tableView extension
+extension ProfileVC: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 6
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 { return 1 }
+        else if section == 1 { return 1 }
+        else if section == 2 { return 1 }
+        else if section == 3 { return 1 }
+        else if section == 4 { return 1 }
+        else { return 0 }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell0 = tableView.dequeueReusableCell(withIdentifier: Cells.phCollectionID, for: indexPath) as? PhotoCollectionTableViewCell else { fatalError("Unable to dequeReusableCell") }
+            cell0.phSet(ph: photoContent[indexPath.item])
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(fingerTap))
+            cell0.addGestureRecognizer(tapGesture)
+            return cell0
+        }
+
+        else if indexPath.section == 1 {
+            guard let cell1 = tableView.dequeueReusableCell(withIdentifier: Cells.postCellID, for: indexPath) as? PostTableViewCell else { fatalError("Unable to dequeReusableCell") }
+            let post = post1[indexPath.row]
+            cell1.set(post: post)
+            return cell1
+        }
+
+        else if indexPath.section == 2 {
+            guard let cell2 = tableView.dequeueReusableCell(withIdentifier: Cells.postCellID, for: indexPath) as? PostTableViewCell else { fatalError("Unable to dequeReusableCell") }
+            let post = post2[indexPath.row]
+            cell2.set(post: post)
+            return cell2
+        }
+
+        else if indexPath.section == 3 {
+            guard let cell3 = tableView.dequeueReusableCell(withIdentifier: Cells.postCellID, for: indexPath) as? PostTableViewCell else { fatalError("Unable to dequeReusableCell") }
+            let post = post3[indexPath.row]
+            cell3.set(post: post)
+            return cell3
+        }
+
+        else if indexPath.section == 4 {
+            guard let cell4 = tableView.dequeueReusableCell(withIdentifier: Cells.postCellID, for: indexPath) as? PostTableViewCell else { fatalError("Unable to dequeReusableCell") }
+            let post = post4[indexPath.row]
+            cell4.set(post: post)
+            return cell4
+        }
+        return UITableViewCell()
+        //let post = postContent[indexPath.row]
+    }
+
+    // MARK: -heightForRowAt (высота ячейки tableView)
+    // photoCollectionView height
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            let height: CGFloat = 120.0
+            return height
+        } else if indexPath.section == 1 {
+            let height: CGFloat = 460.0
+            return height
+        } else if indexPath.section == 2 {
+            let height: CGFloat = 460.0
+            return height
+        } else if indexPath.section == 3 {
+            let height: CGFloat = 460.0
+            return height
+        } else if indexPath.section == 4 {
+            let height: CGFloat = 460.0
+            return height
+        }
+        return CGFloat()
+    }
+}
+    // MARK: - tableView HEADER
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        if section == 0 {
+//            let headerView = ProfileHeaderView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 280))
+//            headerView.backgroundColor = .systemGray6
+//            return headerView
+//        } else if section != 0 {
+//        }
+//        return UIView()
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        // высота хедера
+//        return CGFloat(280)
+//    }
